84c92c38fb5bf778425529d97833e8081558d0f9
dayane07 <dayaanez.9008@gmail.com>
2025-09-05 20:38:42 -0300

    #cidade

---
+++ atv04.py
@@ -0,0 +1,13 @@
+cidadeA = 90.000 
+cidadeB = 250.000
+
+taxaA = 3.5/100
+taxaB = 250.000/100
+anos = 0
+while cidadeA == cidadeB:
+    cidadeA >= cidadeB * taxaA
+    cidadeB <= cidadeA * taxaB
+
+    anos +=1
+    print (anos)
+
