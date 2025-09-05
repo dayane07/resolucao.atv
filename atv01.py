ba65431a029bb1817286432c06f42e9b63b6dccf
dayane07 <dayaanez.9008@gmail.com>
2025-09-05 19:30:18 -0300

    #atv

--- atv01.py
+++ atv01.py
@@ -1,9 +1,6 @@
 nota = (0, 1, 2, 3, 4, 5)
 if nota >= 0 and nota <= 5:
-    int(input("digite a nota:"))
-    print ("digite a nota:")
     print ("nota valida")
-
 else: 
     while nota < 0 or nota > 5: 
         print ("nota invalida")
