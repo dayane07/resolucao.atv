95bf6c6da5556c84b897a69f13ee389f7c6cec4c
dayane07 <dayaanez9008.@gmail.com>
2025-09-06 17:40:13 -0300

    Update atv04.py

--- atv04.py
+++ atv04.py
@@ -1,13 +1,14 @@
-cidadeA = 90.000 
-cidadeB = 250.000
-
-taxaA = 3.5/100
-taxaB = 250.000/100
+populacao_A = 90000
+taxa_crescimento_A = 0.035  # 3.5%
+populacao_B = 250000
+taxa_crescimento_B = 0.012  # 1.2%
 anos = 0
-while cidadeA == cidadeB:
-    cidadeA >= cidadeB * taxaA
-    cidadeB <= cidadeA * taxaB
 
-    anos +=1
-    print (anos)
+while populacao_A < populacao_B:
+    populacao_A += populacao_A * taxa_crescimento_A
+    populacao_B += populacao_B * taxa_crescimento_B
+    anos += 1
 
+print(f"Serão necessários {anos} anos para que a população da cidade A ultrapasse ou iguale a da cidade B.")
+print(f"População final da cidade A: {int(populacao_A)}")
+print(f"População final da cidade B: {int(populacao_B)}")
